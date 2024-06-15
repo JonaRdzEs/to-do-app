cf6cf29040981d97eab5cbbd3998093d1e946582
Jonathan Rodriguez <jonathan.rdzes@gmail.com>
2024-06-14 19:24:38 -0600

    Initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+/*
+  blue: #3a7bfd;
+  check-bg: linear-gradient #57ddff to #c058f3;
+
+  DARK
+  - Very Dark Blue: #161722;
+  - Very Dark Desaturated Blue: #25273c;
+  - Light Grayish Blue: #cacde8;
+  - Light Grayish Blue (hover): #e4e5f1;
+  - Dark Grayish Blue: #777a92;
+  - Very Dark Grayish Blue: #4d5066;
+  - Very Dark Grayish Blue: #393a4c
+
+  LIGHT
+  - Very Light Gray: #fafafa;
+  - Very Light Grayish Blue: #e4e5f1;
+  - Light Grayish Blue: #d2d3db
+  - Dark Grayish Blue: #9394a5
+  - Very Dark Grayish Blue: #484b6a;
+
+*/
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
